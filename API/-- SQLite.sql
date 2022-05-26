493689e6fe4c89b049a86ad29afeb8cc12196234
Vijay Kunduru <kunduruvijay1996@gmail.com>
2022-05-26 13:32:32 -0600

    End of section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Anny");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Charle");
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+using System.Security.AccessControl;
+using System.Xml.Schema;
+using System.Net.Mime;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using API.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Entities;
+using System.Threading.Tasks;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+
+        }
+
+    }
+}
